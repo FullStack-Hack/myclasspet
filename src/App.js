adce1b638161bccffd03b9d6d842aa9169e5b6c6
Teofilo Callanaupa <teofilocallanaupa@gmail.com>
2021-02-28 09:13:17 -0500

    env local works

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
 import "./App.css";
-import {temp} from "./secrets";
 
-// const myurl = temp;
 function App() {
-  console.log(process.env);
   return (
     <div className="App">
       <div>
         test
         <iframe
           title="bot"
-          src={`https://webchat.botframework.com/embed/myclasspet1?s=${temp}`}
+          src={`https://webchat.botframework.com/embed/myclasspet1?s=${process.env.REACT_APP_YOUR_SECRET_HERE}`}
           style={{ minWidth: "400px", width: "100%", minHeight: "500px" }}
         ></iframe>
       </div>
