1cd1f245f243b2b509c3a6238c542d99aa51fce7
Eric Zou <ericczzou@gmail.com>
2021-03-03 17:55:22 -0500

    add singlestudent & singlestudent/activity api routes

---
+++ src/components/SingleStudent.js
@@ -0,0 +1,31 @@
+import React, { useEffect, useState } from "react";
+import FullCalendar from "@fullcalendar/react";
+import dayGridPlugin from "@fullcalendar/daygrid";
+import axios from "axios";
+
+const SingleStudent = () => {
+  const [activities, setActivities] = useState([]);
+
+  //GET activities from selected student
+  //presumably we would pass the studentId down from the 'AllStudents' view
+  useEffect(() => {
+    //need another function for async/await within useEffect
+    //can just call the function when using Redux
+    const getActivities = async () => {
+      //make sure server port and front end port are the same?
+      const { data } = await axios.get(`/api/students/${studentId}/activities`);
+      setActivities(data);
+    };
+    getActivities();
+  }, []);
+
+  return (
+    <FullCalendar
+      plugins={[dayGridPlugin]}
+      initialView="dayGridDay"
+      events={activities}
+    />
+  );
+};
+
+export default SingleStudent;
