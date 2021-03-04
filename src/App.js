f8882bef12deb52f248598bc85b6604b357e7878
kaitbr0 <humansare@gmail.com>
2021-03-04 12:10:07 -0500

    added all students component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,10 @@
 import "./App.css";
 import ChatComponent from "./ChatComponent";
-import React, { useState, useEffect } from "react";
-import axios from "axios";
+import React from "react";
+
+// import AllStudents from "./components/AllStudents";
 
 function App() {
-  const [activities, setActivities] = useState([]);
-
-  //GET activities from selected student
-  //presumably we would pass the studentId down from the 'AllStudents' view
-  useEffect(() => {
-    //need another function for async/await within useEffect
-    //can just call the function when using Redux
-    const getActivities = async () => {
-      //make sure server port and front end port are the same?
-      const { data } = await axios.get(`/api/students/1/activities`);
-      setActivities(data);
-    };
-    getActivities();
-  }, []);
-  console.log(activities);
-
   return (
     <div className="App">
       <div>
