d741bfe92b3cea6f67a767b32be02cb458ae12d4
May <mayyuhein@gmail.com>
2021-03-07 11:06:33 -0500

    [] Navbar restyled

--- src/App.js
+++ src/App.js
@@ -12,8 +12,8 @@ function App() {
       <div>
         <BrowserRouter>
           <Navbar />
-          <ChatComponent />
           <Routes />
+          <ChatComponent />
         </BrowserRouter>
       </div>
     </div>
