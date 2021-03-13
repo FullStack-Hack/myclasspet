2fa8650e818bffb9ed3b3b46cbb4d3ad7a6e17c7
Teofilo Callanaupa <teofilocallanaupa@gmail.com>
2021-03-13 12:29:06 -0500

    test

--- server/db/db.js
+++ server/db/db.js
@@ -4,7 +4,7 @@ const pkg = require("../../package.json");
 const databaseName =
   pkg.name + (process.env.NODE_ENV === "test" ? "-test" : "");
 // moved from server/db/db.js to account for use of db or db-test
-process.env.DATABASE_URL = `postgres://localhost:5432/${databaseName}`;
+// process.env.DATABASE_URL = `postgres://localhost:5432/${databaseName}`;
 
 const db = new Sequelize(process.env.DATABASE_URL, {
   logging: false,
