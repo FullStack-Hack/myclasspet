46497c5784731dea38fe37f260e92f66d7427e1b
Teofilo Callanaupa <teofilocallanaupa@gmail.com>
2021-03-13 12:43:24 -0500

    ssl

--- server/db/db.js
+++ server/db/db.js
@@ -6,7 +6,24 @@ const databaseName =
 // moved from server/db/db.js to account for use of db or db-test
 // process.env.DATABASE_URL = `postgres://localhost:5432/${databaseName}`;
 
-const db = new Sequelize(process.env.DATABASE_URL, {
-  logging: false,
-});
+// const db = new Sequelize(process.env.DATABASE_URL, {
+//   logging: false,
+// });
+
+const db = new Sequelize(
+  process.env.DATABASE_URL || `postgres://localhost:5432/${databaseName}`,
+  {
+    logging: false,
+    operatorAliases: false,
+    dialect: 'postgres',
+    protocol: 'postgres',
+    ssl: true,
+    dialectOptions: {
+      ssl: process.env.DATABASE_URL && {
+        require: true
+      }
+    }
+  }
+)
+
 module.exports = db;
